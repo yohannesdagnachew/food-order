3010f63014e44aaa9067b14e47b9272c5ab0dfa8
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-11-25 16:29:17 +0300

    Add CSS header and Add Cart icon

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,14 @@
 import { Fragment } from 'react';
 
 import HeaderCartButton from './HeaderCartButton';
-import mealsImage from '../../assets/meals.jpg';
+import mealsImage from '../../assets/meals.jpeg';
 import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>ReactMeals</h1>
+        <h1>PortMeals</h1>
         <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
