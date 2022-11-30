05176ce61716a32b621e7667be7e9eb5a7b4d209
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-11-30 10:07:35 +0300

    Add model state

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,8 +8,8 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>PortMeals</h1>
-        <HeaderCartButton />
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food!' />
